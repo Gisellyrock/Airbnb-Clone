e20172d014485b16442dd3f13ec430a1820bcfba
GisellyRock <gisellydsr@gmail.com>
2023-11-24 19:32:44 -0300

    feat:Counter

--- app/search/Header.jsx
+++ app/search/Header.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from 'react';
+import SearchBar from './components/SearchBar';
 
 const toggleExpanded = () => {
   setIsExpanded((prevIsExpanded) => !prevIsExpanded);
@@ -10,21 +11,25 @@ export default function Header() {
   return (
     <header className="container flex justify-between h-10 border-b bg-white z-50 fixed w-full">
       <div className="text-red-500">airbnb</div>
-      <button
-        onClick={toggleExpanded}
-        className="search-container flex gap-3 rounded-lg"
-      >
-        <div className="input border-r">
-          <p>Anywhere</p>
-        </div>
-        <div className="input border-r">
-          <p>Any Date</p>
-        </div>
-        <div className="input border-r">
-          <p>Add Guests</p>
-        </div>
-        <div className="search-btn">Search</div>
-      </button>
+      {isExpanded ? (
+        <SearchBar />
+      ) : (
+        <button
+          onClick={toggleExpanded}
+          className="search-container flex gap-3 rounded-lg"
+        >
+          <div className="input border-r">
+            <p>Anywhere</p>
+          </div>
+          <div className="input border-r">
+            <p>Any Date</p>
+          </div>
+          <div className="input border-r">
+            <p>Add Guests</p>
+          </div>
+          <div className="search-btn">Search</div>
+        </button>
+      )}
       <div>user</div>
     </header>
   );
--- app/search/components/SearchBar.jsx
+++ app/search/components/SearchBar.jsx
@@ -1,13 +1,45 @@
-const Search = () => {
+import { useState } from 'react';
+
+const SearchBar = () => {
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+
   return (
-    <div>
-      <button>
-        <p>Search destinations</p>
+    <div className="flex flex-row self-center rounded-full border p-2 mt-8 w-3/4">
+      <button onClick={() => setIsSearchFocused(true)}>
+        <p className="font-bold">Where</p>
+        {isSearchFocused ? (
+          <input
+            type="text"
+            placeholder="Search destinations"
+            className="text-slate-800 bg-transparent border-none outline-none"
+          />
+        ) : (
+          <p className="text-slate-600">Search destinations</p>
+        )}
       </button>
-      <div>
-        <label>
-          <p></p>
+      <div className="dropdown dropdwn-end px-4 border-r">
+        <label tabIndex={1}>
+          <p className="font-bold">Dates</p>
+          <p className="text-slate-600">Select Ranged</p>
         </label>
+        <div
+          tabIndex={1}
+          className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <p>Range Selecter</p>
+        </div>
+      </div>
+      <div className="dropdown dropdown-end px-4">
+        <label tabIndex={2}>
+          <p className="font-bold">Who</p>
+          <p className="font-bold">Add Guests</p>
+        </label>
+        <div
+          tabIndex={2}
+          className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <p>Counter</p>
+        </div>
       </div>
     </div>
   );
