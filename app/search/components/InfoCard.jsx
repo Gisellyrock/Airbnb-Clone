e754395e823b1f7b53d2bac75e350d8a48b20e29
GisellyRock <gisellydsr@gmail.com>
2023-11-25 11:08:50 -0300

    feat:InfoCard

---
+++ app/search/components/InfoCard.jsx
@@ -0,0 +1,5 @@
+'use client';
+import { useState } from 'react';
+import { HeartIcon } from '@heroicons/react/outline';
+import { HeartIcon as FilledHeartIcon } from '@heroicons/react/solid';
+import { StarIcon } from '@heroicons/react/solid';
--- app/search/page.js
+++ app/search/page.js
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <h1>My Page</h1>;
+export default async function Page() {
+  const res = await fetch('http://localhost:3000/api/search');
+  const data = await res.json();
+
+  return (
+    <>
+      {data.map((listing) => (
+        <p key={listing.id}>{listing.id}</p>
+      ))}
+    </>
+  );
 }
