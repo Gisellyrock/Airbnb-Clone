60ee655b09384fd4497e48f1e8666c565468e0f6
GisellyRock <gisellydsr@gmail.com>
2023-11-25 20:19:02 -0300

    fix:ResultsList Page

--- app/search/page.js
+++ app/search/page.js
@@ -1,4 +1,5 @@
 import { ResultsList } from './results/ResultsList';
+import Link from 'next/link';
 
 export default async function Page() {
   const res = await fetch('http://localhost:3000/api/search');
@@ -6,6 +7,14 @@ export default async function Page() {
 
   return (
     <>
+      <div className="flex items-center justify-center w-screen bg-cover bg-center h-48 bg-[url('/images/booking.jpg')]">
+        <Link
+          href="/search/results"
+          className="rounded-full bg-[#045C67] hover:bg-[#0f7381] px-4 py-2 text-white"
+        >
+          Browse Stays
+        </Link>
+      </div>
       <ResultsList data={data} />
     </>
   );
